ac2397f0a21f375206018caa34437214b0c0516d
Joshua Mallett <Joshua_Mallett@spra.com>
2019-10-16 13:24:26 -0700

    map code testing as a group

---
+++ map_code.R
@@ -0,0 +1,42 @@
+getwd()
+setwd("P:/")
+
+read.delim("fireball.txt")
+
+fireball<-read.delim("fireball.txt")
+
+head(fireball)
+names(fireball)
+summary(fireball)
+summary(fireball$latitude)
+
+install.packages("rworldmap")
+library(rworldmap)
+newmap <- getMap(resolution = "low")
+plot(newmap)
+points(fireball$longitude,fireball$latitude, col = "red")
+
+#ggplot, alpha is plot transparency, CLEAN MAP
+library(ggplot2)
+world_map <- map_data("world")
+p <- ggplot() + coord_fixed() + xlab("") + ylab("")
+
+base_world_messy <- p + geom_polygon(data=world_map, aes(x=long, y=lat, group=group), 
+                                     colour="blue", fill="light green")
+cleanup <- 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+        panel.background = element_rect(fill = 'white', colour = 'white'), 
+        axis.line = element_line(colour = "white"), legend.position="none",
+        axis.ticks=element_blank(), axis.text.x=element_blank(),
+        axis.text.y=element_blank())
+
+base_world <- base_world_messy + cleanup
+
+#convert zipcode to latitude longitude
+install.packages("zipcode")
+library(zipcode)
+
+zipcode<-c(94612,85050,48820,94618,94070)
+fakezips <- data.frame(zipcode)
+
+data(zipcode)
